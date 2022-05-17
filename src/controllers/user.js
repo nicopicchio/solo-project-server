229884be645b1109c40563fcffb4b035a8e7224e
Nico Picchio <nicolapicchio@gmail.com>
2022-05-17 16:50:59 +0100

    user can register an account and login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,53 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+const secretKey = process.env.SECRET_KEY;
+const errors = {
+	server: {
+		code: 500,
+		message: 'Ooooooops! Something went wrong!',
+	},
+	access: {
+		code: 401,
+		message: 'Invalid username or password',
+	},
+};
+
+const saltRounds = 10;
+const startingBalance = 0;
 
 export const register = async (req, res) => {
-  console.log('register')
-}
+	try {
+		const registeredUser = await prisma.user.create({
+			data: {
+				username: req.body.username,
+				forename: req.body.forename,
+				surname: req.body.surname,
+				password: await bcrypt.hash(req.body.password, saltRounds),
+				balance: startingBalance,
+			},
+		});
+		res.status(200).json({ registeredUser });
+	} catch (err) {
+		console.error(err);
+		res.status(errors.server.code).json(errors.server.message);
+	}
+};
 
 export const login = async (req, res) => {
-  console.log('login')
-}
+	const matchingUser = await prisma.user.findUnique({
+		where: { username: req.body.username },
+	});
+	const matchingPassword = await bcrypt.compare(
+		req.body.password,
+		matchingUser.password
+	);
+	if (matchingUser && matchingPassword) {
+		res
+			.status(200)
+			.json(jwt.sign({ username: matchingUser.username }, secretKey));
+		return;
+	}
+	res.status(errors.access.code).json(errors.access.message);
+};
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import { register, login } from '../controllers/user.js';
 
-router.get('/register', register);
+router.post('/register', register);
 router.post('/login', login);
 
 export default router;
