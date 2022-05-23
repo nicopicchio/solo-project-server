44ce913f156a1bdbe8f6936504ebf611e12af5b2
Nico Picchio <nicolapicchio@gmail.com>
2022-05-23 14:34:50 +0100

    modified api call, due to issue with data received

--- src/controllers/fugitives.js
+++ src/controllers/fugitives.js
@@ -6,27 +6,30 @@ export const getAllFugitives = async (req, res) => {
 	try {
 		const response = await axios.get(FBI_URL);
 		const fugitivesArray = response.data.items;
-		const filteredFugitives = fugitivesArray.map((fugitive) => {
-			if (fugitive.person_classification === 'Main' || fugitive.reward_text !== null) {
-				const fugitiveObject = {
-					name: fugitive.title,
-					nationality: fugitive.nationality,
-					id: fugitive.uid,
-					url: fugitive.url,
-					warning: fugitive.warning_message,
-					images: fugitive.images,
-					files: fugitive.files,
-					reward: fugitive.reward_text,
-					caution: fugitive.caution,
-					remarks: fugitive.remarks,
-					status: fugitive.status,
-				}
-				return fugitiveObject
-			}
-		})
-		res.json(filteredFugitives);
+		const filteredFugitives = fugitivesArray.filter(
+			(fugitive) =>
+				fugitive.person_classification === 'Main' ||
+				fugitive.reward_text !== null
+		);
+		const mappedFugitives = filteredFugitives.map((fugitive) => {
+			const fugitiveObject = {
+				name: fugitive.title,
+				nationality: fugitive.nationality,
+				id: fugitive.uid,
+				url: fugitive.url,
+				warning: fugitive.warning_message,
+				images: fugitive.images,
+				files: fugitive.files,
+				reward: fugitive.reward_text,
+				caution: fugitive.caution,
+				remarks: fugitive.remarks,
+				status: fugitive.status,
+			};
+			return fugitiveObject;
+		});
+		res.json(mappedFugitives);
 	} catch (error) {
 		console.error(error);
-		res.status()
+		res.status();
 	}
 };
