d6db50ddd8428ff07f508c161255a60583c1896a
Nico Picchio <nicolapicchio@gmail.com>
2022-05-24 12:20:00 +0100

    solved bug affecting reward amount

--- src/controllers/fugitives.js
+++ src/controllers/fugitives.js
@@ -22,7 +22,7 @@ export const getAllFugitives = async (req, res) => {
 				images: fugitive.images,
 				files: fugitive.files,
 				reward: fugitive.reward_text,
-				rewardAmount: fugitive.reward_text.replace(/\D/g, ''),
+				rewardAmount: fugitive.reward_text.match(/\$((?:\d|\,)*\.?\d+)/g),
 				caution: fugitive.caution,
 				remarks: fugitive.remarks,
 				status: fugitive.status,
