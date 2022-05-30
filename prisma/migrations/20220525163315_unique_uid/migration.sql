e4ceaf09b1cd027085b36944faae5fc3e6b07712
Nico Picchio <nicolapicchio@gmail.com>
2022-05-30 14:43:08 +0100

    changed db provider

--- prisma/migrations/20220525163315_unique_uid/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[uid]` on the table `Job` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Job_uid_key" ON "Job"("uid");
--- prisma/migrations/20220530134233_init/migration.sql
+++ prisma/migrations/20220530134233_init/migration.sql
@@ -13,16 +13,22 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Target" (
+CREATE TABLE "Job" (
     "id" SERIAL NOT NULL,
-    "fbiId" TEXT NOT NULL,
+    "uid" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Target_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Job_uid_key" ON "Job"("uid");
+
 -- AddForeignKey
-ALTER TABLE "Target" ADD CONSTRAINT "Target_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
